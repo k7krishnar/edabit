9a4764fdee221073e05830a1512d5d082f263a59
k7krishnar <888krishna@gmail.com>
2019-12-24 20:23:52 +0530

    v1

---
+++ no_duplicate_letters.py
@@ -0,0 +1,17 @@
+def no_duplicate_letters(phrase):
+    flag=True
+    for word in phrase.split(' '):
+        print(word) # for ref
+        if len(word) > len(set(word)):
+            flag=False
+    print(flag) # for ref
+    return flag
+
+no_duplicate_letters("Easy does it.")
+no_duplicate_letters("So far, so good.")
+
+
+#other
+
+def no_duplicate_letters(phrase):
+    return all([len(set(word))==len(word) for word in phrase.lower().split(' ')])
